d5416a44257ad27b93c3d073d8620d4e3557cd55
SamLyons <SamLyons@Samuels-MacBook-Air.local>
2022-10-11 17:52:17 -0400

    added functionality to cart modal

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ $(document).ready(function() {
         $("#thankModal").modal('hide');
         $("#confirmModal").modal('hide');
     }));
+
 });
 
 let orderTime = document.querySelector('#orderTime');
@@ -33,6 +34,37 @@ let twelveHour = today.getHours() % 12 || 12;
 let leadMin = '';
 let minutes = today.getMinutes();
 let timeOfDay = '';
+let updateCartButton = document.querySelector(".updateCartButton");
+let hotCoffeeAmount = document.querySelector("#hotCoffeeAmount")
+let icedCoffeeAmount = document.querySelector("#icedCoffeeAmount")
+let foodAmount = document.querySelector("#foodAmount")
+let total = document.querySelector("#total")
+let placeOrderButton = document.querySelector(".placeOrderButton")
+
+
+placeOrderButton.addEventListener('click', function(){
+    hotCoffeeAmount.value = 0
+    icedCoffeeAmount.value = 0
+    foodAmount.value = 0
+    total.textContent = `Your Total is`
+})
+
+function getAmount(amount, price){
+    total1 = (amount * price)
+    return total1
+}
+
+
+updateCartButton.addEventListener('click', function(){
+    let hotCoffeeNumber = hotCoffeeAmount.value
+    let icedCoffeeNumber = icedCoffeeAmount.value
+    let foodNumber = foodAmount.value
+    let hotTotal = getAmount(hotCoffeeNumber, 3.00)
+    let icedTotal = getAmount(icedCoffeeNumber, 4.00)
+    let foodTotal = getAmount(foodNumber, 6.00)
+    let totalPrice = parseInt(hotTotal + icedTotal + foodTotal)
+    total.textContent = `Your total is $${totalPrice}`
+})
 
 
 if (minutes < 10){
